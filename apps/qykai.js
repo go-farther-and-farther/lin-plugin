448cb1143f5c3688cf8f4694591245a403200c1d
越追越远 <2859167710@qq.com>
2022-08-27 07:34:38 +0000

    update apps/qykai.js. 支持选择是否只关注@信息
    Signed-off-by: 越追越远 <2859167710@qq.com>

--- apps/qykai.js
+++ apps/qykai.js
@@ -3,10 +3,10 @@ import fetch from "node-fetch";
 //感谢前边所有版本的青云客作者
 //@苏苏@dmhfd(1695037643);
 //项目路径
-const _path = process.cwd();
 var BotName = "空之荧";//你家机器人叫这个，记得改了
 var gailv = 0.2;//概率20%，这个是初始概率，重启后就是这个概率
 var gailv_ = 0.1;//每次条件的概率
+const onlyReplyAt = true //群聊是否只关注@信息
 //简单应用示例
 //1.定义命令规则
 export class qykai extends plugin {
@@ -30,18 +30,16 @@ export class qykai extends plugin {
 			]
 		})
 	}
-
 	/**
 	 * 
 	 * @param e oicq传递的事件参数e
 	 */
 	async qyk(e) {
+		if (!e.msg || e.msg.charAt(0) == '#') return;
 		//e.msg 用户的命令消息
 		console.log("用户命令：", e.msg);
-		//越追越远添加----------------------------------------------------------------------------------
 		//控制ai回复概率的模块
 		let j = Math.random();
-
 		if (e.msg.includes('太安静')) {
 			//如果概率等于1
 			if (gailv > 0.99) {
@@ -69,7 +67,7 @@ export class qykai extends plugin {
 			let gailvs = gailv * 100;
 			//保留整数
 			gailvs = gailvs.toFixed(0);
-			e.reply(`可以说“太吵了”，“太安静了”调节\n目前青云客ai触发概率：${gailvs}%`)
+			e.reply(`可以输入“太吵了”、“太安静了”或者进入文件修改是否只关注@信息来调节\n目前青云客ai触发概率：${gailvs}%，是否只关注@信息：${onlyReplyAt}`)
 			return true;
 		}
 		if (j >= gailv)//ai的触发率
@@ -77,9 +75,7 @@ export class qykai extends plugin {
 			console.log("退出青云客ai");
 			return true;
 		}
-		//越追越远添加----------------------------------------------------------------------------------
-		if (!e.msg || e.msg.charAt(0) == '#') return;
-		if (e.msg.includes(BotName) || (e.at && e.at == BotConfig.account.qq) || e.isPrivate) {
+		if (e.msg.includes(BotName) || (e.at && e.at == BotConfig.account.qq) || e.isPrivate || !onlyReplyAt) {
 			console.log("青云客消息：", e.msg);
 			let Msg = e.msg.replace(BotName, "菲菲");
 			let url = `http://api.qingyunke.com/api.php?key=free&appid=0&msg=${Msg}`;
