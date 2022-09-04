01adbe943f788139364298fe7b53c772c03f5287
越追越远 <2859167710@qq.com>
2022-09-04 09:09:37 +0000

    update apps/exercise.js.重写
    Signed-off-by: 越追越远 <2859167710@qq.com>

--- apps/exercise.js
+++ apps/exercise.js
@@ -4,6 +4,7 @@ import fs from "fs";
 import schedule from "node-schedule";
 //项目路径
 let exerciseCD = {};
+let exerciseCD_ = {};
 //如果报错请删除Yunzai/data/目录中lin文件夹
 const dirpath = "data/lin/";//文件夹路径
 var filename = `battle`;//文件名
@@ -17,12 +18,13 @@ let Template = {//创建该用户
     "Privilege": 0,
 };
 //配置一些有意思的参数
-let Cooling_time2 = 300 //命令间隔时间，单位分钟，这是锻炼的冷却时间#初始为300分钟
-export class exercise extends plugin {//锻炼
+let Cooling_time2 = 300 //命令间隔时间，单位分钟，这是修炼的冷却时间#初始为300分钟
+let Cooling_time3 = 30 //命令间隔时间，单位分钟，这是突破的冷却时间#初始为30分钟
+export class exercise extends plugin {//修炼
     constructor() {
         super({
             /** 功能名称 */
-            name: '锻炼',
+            name: '修炼',
             /** 功能描述 */
             dsc: '',
             /** https://oicqjs.github.io/oicq/#events */
@@ -32,22 +34,28 @@ export class exercise extends plugin {//锻炼
             rule: [
                 {
                     /** 命令正则匹配 */
-                    reg: "^#(发起|开始)?(锻炼|早睡)(.*)$", //匹配消息正则，命令正则
+                    reg: "^#(发起|开始)?(锻炼|早睡|修炼)(.*)$", //匹配消息正则，命令正则
                     /** 执行方法 */
                     fnc: 'exercise'
+                },
+                {
+                    /** 命令正则匹配 */
+                    reg: "^#突破$", //匹配消息正则，命令正则
+                    /** 执行方法 */
+                    fnc: 'exercise_'
                 }
             ]
         })
     }
-	/**
-	 * 
-	 * @param e oicq传递的事件参数e
-	 */
-    async exercise(e) {
-        console.log("用户命令：", e.msg);
+    /**
+     * 
+     * @param e oicq传递的事件参数e
+     */
+    async exercise_(e) {
+        console.log("用户命令：", e.msg); console.log("用户命令：", e.msg);
         let user_id = e.user_id;
-        if (exerciseCD[e.user_id]) { //判定是否在冷却中
-            e.reply(`你刚刚进行了一次锻炼，请耐心一点，等待${Cooling_time2}分钟后再次锻炼吧！`);
+        if (exerciseCD_[user_id]) { //判定是否在冷却中
+            e.reply(`你刚刚进行了一次突破，请耐心一点，等待${Cooling_time3}分钟后再次突破吧！`);
             return;
         }
         if (!fs.existsSync(dirpath)) {//如果文件夹不存在
@@ -58,13 +66,146 @@ export class exercise extends plugin {//锻炼
             }));
         }
         const json = JSON.parse(fs.readFileSync(dirpath + "/" + filename));//读取文件
-        if (!json.hasOwnProperty(e.user_id)) {//如果json中不存在该用户
-            json[e.user_id] = Template
+        if (!json.hasOwnProperty(user_id)) {//如果json中不存在该用户
+            json[user_id] = Template
         }
-        exerciseCD[e.user_id] = true;
-        exerciseCD[e.user_id] = setTimeout(() => {//冷却时间
-            if (exerciseCD[e.user_id]) {
-                delete exerciseCD[e.user_id];
+        exerciseCD[user_id] = true;
+        exerciseCD[user_id] = setTimeout(() => {//冷却时间
+            if (exerciseCD_[user_id]) {
+                delete exerciseCD_[user_id];
+            }
+        }, Cooling_time3 * 1000 * 60);
+        if (json[user_id].energy < 1) {
+            json[user_id].energy = 0
+        }//当战斗力小于1时，自动归零
+
+        if (json[user_id].energy < 5) json[user_id].level = 0
+        else if (json[user_id].energy < 10 && json[user_id].level >= 1) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 20 && json[user_id].level >= 2) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 30 && json[user_id].level >= 3) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 40 && json[user_id].level >= 4) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+
+        else if (json[user_id].energy < 65 && json[user_id].level >= 5) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 70 && json[user_id].level >= 6) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 85 && json[user_id].level >= 7) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 100 && json[user_id].level >= 8) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+
+        else if (json[user_id].energy < 125 && json[user_id].level >= 9) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 150 && json[user_id].level >= 10) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 175 && json[user_id].level >= 11) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 200 && json[user_id].level >= 12) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+
+        else if (json[user_id].energy < 230 && json[user_id].level >= 13) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 260 && json[user_id].level >= 14) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 290 && json[user_id].level >= 15) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy < 320 && json[user_id].level >= 16) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        else if (json[user_id].energy >= 320 && json[user_id].level < 16 + (json[user_id].energy - 320) / 80) {
+            e.reply('修为不足,请再接再厉')
+            return
+        }
+        let gailv = 100 - json[user_id.level] * 5
+        let i = Math.random() * 100
+        if (i < gailv) {
+            e.reply('突破失败，请努力修行')
+            return
+        }
+        json[user_id].energy++
+        if (json[user_id].level == 0) json[user_id].levels = '无境界'
+        else if (json[user_id].level == 1) json[user_id].levels = '小乘境初期'
+        else if (json[user_id].level == 2) json[user_id].levels = '小乘境中期'
+        else if (json[user_id].level == 3) json[user_id].levels = '小乘境后期'
+        else if (json[user_id].level == 4) json[user_id].levels = '小乘境巅峰'
+        else if (json[user_id].level == 5) json[user_id].levels = '大乘境初期'
+        else if (json[user_id].level == 6) json[user_id].levels = '大乘境中期'
+        else if (json[user_id].level == 7) json[user_id].levels = '大乘境后期'
+        else if (json[user_id].level == 8) json[user_id].levels = '大乘境巅峰'
+        else if (json[user_id].level == 9) json[user_id].levels = '宗师境初期'
+        else if (json[user_id].level == 10) json[user_id].levels = '宗师境中期'
+        else if (json[user_id].level == 11) json[user_id].levels = '宗师境后期'
+        else if (json[user_id].level == 12) json[user_id].levels = '宗师境巅峰'
+        else if (json[user_id].level == 13) json[user_id].levels = '至臻境初期'
+        else if (json[user_id].level == 14) json[user_id].levels = '至臻境中期'
+        else if (json[user_id].level == 15) json[user_id].levels = '至臻境后期'
+        else if (json[user_id].level == 16) json[user_id].levels = '至臻境巅峰'
+        else if (json[user_id].level > 16) json[user_id].levels = '返璞归真'
+        e.reply(`突破成功，当前境界${json[user_id].levels}`)
+        fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(json, null, "\t"));//写入文件
+        return true;
+    }
+    /**
+     * 
+     * @param e oicq传递的事件参数e
+     */
+    async exercise(e) {
+        console.log("用户命令：", e.msg);
+        let user_id = e.user_id;
+        if (exerciseCD[user_id]) { //判定是否在冷却中
+            e.reply(`你刚刚进行了一次修炼，请耐心一点，等待${Cooling_time2}分钟后再次修炼吧！`);
+            return;
+        }
+        if (!fs.existsSync(dirpath)) {//如果文件夹不存在
+            fs.mkdirSync(dirpath);//创建文件夹
+        }
+        if (!fs.existsSync(dirpath + "/" + filename)) {//如果文件不存在
+            fs.writeFileSync(dirpath + "/" + filename, JSON.stringify({//创建文件
+            }));
+        }
+        const json = JSON.parse(fs.readFileSync(dirpath + "/" + filename));//读取文件
+        if (!json.hasOwnProperty(user_id)) {//如果json中不存在该用户
+            json[user_id] = Template
+        }
+        exerciseCD[user_id] = true;
+        exerciseCD[user_id] = setTimeout(() => {//冷却时间
+            if (exerciseCD[user_id]) {
+                delete exerciseCD[user_id];
             }
         }, Cooling_time2 * 1000 * 60);
         const date = new Date();
@@ -73,23 +214,23 @@ export class exercise extends plugin {//锻炼
         if (hours >= 6 && hours <= 8) {
             energy_ = Math.round(3 + 2 * Math.random())
             json[user_id].energy += energy_
-            e.reply([segment.at(e.user_id),
-            `\n🎉恭喜你获得了${energy_}点战斗力,一日之计在于晨，清晨锻炼效果更好哦！\n你的战斗力为:${json[user_id].energy}\n你的境界为${json[user_id].levels}`]);//发送消息
+            e.reply([segment.at(user_id),
+            `\n🎉恭喜你获得了${energy_}点战斗力,一日之计在于晨，清晨修炼效果更好哦！\n你的战斗力为:${json[user_id].energy}\n你的境界为${json[user_id].levels}`]);//发送消息
         } else if (hours >= 8 && hours <= 20) {
             energy_ = Math.round(1 + 2 * Math.random())
             json[user_id].energy += energy_
-            e.reply([segment.at(e.user_id),
+            e.reply([segment.at(user_id),
             `\n🎉恭喜你获得了${energy_}点战斗力！\n你的战斗力为:${json[user_id].energy}\n你的境界为${json[user_id].levels}`]);//发送消息
         } else if (hours >= 20 && hours <= 22 && e.msg.includes('早睡')) {
             e.group.muteMember(user_id, 60 * 60 * 8); //禁言
             energy_ = Math.round(3 + 3 * Math.random())
             json[user_id].energy += energy_
-            e.reply([segment.at(e.user_id),
+            e.reply([segment.at(user_id),
             `\n🎉早睡早起好习惯，恭喜你获得了${energy_}点战斗力！\n你的战斗力为:${json[user_id].energy}\n你的境界为${json[user_id].levels}`]);//发送消息
         } else {
             energy_ = 1
             json[user_id].energy += energy_
-            e.reply([segment.at(e.user_id),
+            e.reply([segment.at(user_id),
             `\n由于睡太晚，你只获得了${energy_}点战斗力！\n你的战斗力为:${json[user_id].energy}\n你的境界为${json[user_id].levels}`]);//发送消息
         }
         fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(json, null, "\t"));//写入文件
@@ -111,78 +252,6 @@ schedule.scheduleJob('0 0 4 * * *', function () {//每日战斗力-1
         }
         if (json[key].energy >= 1) {
             json[key].energy--
-        }
-        if (json[key].energy < 15) {
-            json[key].level = 0
-            json[key].levels = '无境界'
-        }
-        else if (json[key].energy < 30) {
-            json[key].level = 1
-            json[key].levels = '小乘境初期'
-        }
-        else if (json[key].energy < 45) {
-            json[key].level = 2
-            json[key].levels = '小乘境中期'
-        }
-        else if (json[key].energy < 55) {
-            json[key].level = 3
-            json[key].levels = '小乘境后期'
-        }
-        else if (json[key].energy < 60) {
-            json[key].level = 3
-            json[key].levels = '小乘境巅峰'
-        }
-        else if (json[key].energy < 80) {
-            json[key].level = 4
-            json[key].levels = '大乘境初期'
-        }
-        else if (json[key].energy < 100) {
-            json[key].level = 5
-            json[key].levels = '大乘境中期'
-        }
-        else if (json[key].energy < 110) {
-            json[key].level = 6
-            json[key].levels = '大乘境后期'
-        }
-        else if (json[key].energy < 120) {
-            json[key].level = 6
-            json[key].levels = '大乘境巅峰'
-        }
-        else if (json[key].energy < 145) {
-            json[key].level = 7
-            json[key].levels = '宗师境初期'
-        }
-        else if (json[key].energy < 170) {
-            json[key].level = 8
-            json[key].levels = '宗师境中期'
-        }
-        else if (json[key].energy < 190) {
-            json[key].level = 9
-            json[key].levels = '宗师境后期'
-        }
-        else if (json[key].energy < 200) {
-            json[key].level = 9
-            json[key].levels = '宗师境巅峰'
-        }
-        else if (json[key].energy < 240) {
-            json[key].level = 10
-            json[key].levels = '至臻境初期'
-        }
-        else if (json[key].energy < 280) {
-            json[key].level = 11
-            json[key].levels = '至臻境中期'
-        }
-        else if (json[key].energy < 300) {
-            json[key].level = 12
-            json[key].levels = '至臻境后期'
-        }
-        else if (json[key].energy < 320) {
-            json[key].level = 12
-            json[key].levels = '至臻境巅峰'
-        }
-        else {
-            json[key].level = 13
-            json[key].levels = '返璞归真'
         }
     }
     fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(json, null, "\t"));//写入文件
