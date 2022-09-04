25aa65df18036a7a896c17051b50b3f5f8d930ad
越追越远 <2859167710@qq.com>
2022-09-04 09:48:22 +0000

    update apps/help.js.
    Signed-off-by: 越追越远 <2859167710@qq.com>

--- apps/help.js
+++ apps/help.js
@@ -1,5 +1,6 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import fs from "fs";
+import cfg from '../../lib/config/config.js'
 //项目路径
 //如果报错请删除Yunzai/data/目录中lin文件夹
 const dirpath = "data/lin/";//文件夹路径
@@ -42,9 +43,31 @@ export class help extends plugin {
 	 */
 	async rules(e) {
 		e.reply(`决斗指令：#御前决斗@...\n#(锻炼|早睡)\n#我的境界\n#(设置|移除)半步管理员@...\n#(游戏|决斗)规则`)
+		//let msg1 = "lin插件包开始自动更新,不需要该功能可以手动注释"
+		for (let i of cfg.masterQQ) { //这里定义发送给所有主人
+			let userId = i
+			//Bot.pickUser(userId).sendMsg(msg1)
+		}
+		var ls = exec(command, { cwd: `${_path}/plugins/lin/` }, async function (error, stdout, stderr) {
+			let isChanges = error.toString().includes("Your local changes to the following files would be overwritten by merge") ? true : false;
+
+			let isNetwork = error.toString().includes("fatal: unable to access") ? true : false;
+
+			if (isChanges) {
+				let msg2 = "发现新版本！自动更新失败！\nError code: " +
+					error.code +
+					"\n" +
+					error.stack +
+					"\n\n本地代码与远程代码存在冲突,上面报错信息中包含冲突文件名称及路径，请尝试处理冲突\n如果不想保存本地修改请使用【#强制更新】\n(注意：强制更新命令会忽略所有本地对lin插件本身文件的修改，本地修改均不会保存，请注意备份)"
+				for (let i of cfg.masterQQ) { //这里定义发送给所有主人
+					let userId = i
+					Bot.pickUser(userId).sendMsg(msg2)
+				}
+			}
+		});
 		return
 	}
-    /**
+	/**
 	 * 
 	 * @param e oicq传递的事件参数e
 	 */
