f7c1284fb39791fd87b1d03373835932991d9f1d
地球生物 <11464039+jiang-zhitao-1@user.noreply.gitee.com>
2022-09-14 14:33:32 +0000

    update apps/shareMusic.js.
    Signed-off-by: 地球生物 <11464039+jiang-zhitao-1@user.noreply.gitee.com>

--- apps/shareMusic.js
+++ apps/shareMusic.js
@@ -12,6 +12,8 @@ let kg = false
 let wy = false
 let qq = false
 let zt = 0
+let mid = []
+let lb = 4
 export class shareMusic extends plugin {
   constructor () {
     super({
@@ -24,53 +26,16 @@ export class shareMusic extends plugin {
           reg: '^#*(点歌|qq|QQ|kugou|酷狗|网易云|网抑云|网易)(.*)|#听[1-9][0-9]|#听[0-9]*$$',
           fnc: 'shareMusic'
         },
-		{
-			reg:"^#*点动漫(.*)$",
-			fnc:'kanpian'
-		}
+		
 
       ]
     })
   }
 
-async kanpian(e){
-	let msg3 = ""
-	let  k = ""
-	let data3 = ""
-	if(e.msg.includes("点动漫")){
-		
-		k = e.msg.replace(/#点动漫/g,"").trim()
-	}
-	let url = 'https://search.bilibili.com/all?vt=92884847&keyword=' +  k  +  '&from_source=webtop_search&spm_id_from=333.1007'
-	
-	
-	  let response = await fetch(url);
-        const data= await response.text()
-		
-		let data2 = data.match(/href="(\S*) title/g); 
-		
-		
-		
-		console.log(data2)
-		data3 = data2[0].replace(/href=/g,"")
-		
-		
-		data3 = data3.replace(/\"/g, "");
-		data3 = data3.replace(/title/g,"")
-		console.log(data3)
-  
-
-msg3 = "https://www.bavei.com/vip/?url=" + "https:"  + data3 
-  e.reply(msg3);
-  e.reply(["以上是" + k + "的信息，请到浏览器中打开"]);
-  return true;//返回true 阻挡消息不再往下
-}
-
-
 
  async shareMusic(e) {
   const urlList = {
-  qq: 'https://c.y.qq.com/soso/fcgi-bin/client_search_cp?g_tk=5381&p=1&n=20&w=paramsSearch&format=json&loginUin=0&hostUin=0&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&remoteplace=txt.yqq.song&t=0&aggr=1&cr=1&catZhida=1&flag_qc=0',
+  qq: 'https://ovooa.com/API/QQ_Music/?Skey=&uin=&msg=paramsSearch&n=',
   kugou:
     'http://mobilecdn.kugou.com/api/v3/search/song?format=json&keyword=paramsSearch&page=1&pagesize=20&showtype=1',
   wangyiyun: 'https://autumnfish.cn/search?keywords=paramsSearch',//备用API：http://www.clearfor.xyz:3000/cloudsearch?keywords=paramsSearch
@@ -78,8 +43,9 @@ msg3 = "https://www.bavei.com/vip/?url=" + "https:"  + data3
 
   logger.info('[用户命令]', e.msg)
   let msg = e.msg.replace(/\s*/g, "");
+  msg = msg.replace(/#/g,"").trim()
   let isQQReg = new RegExp("^[非VIP]*点歌*(qq|QQ)(.*)$");
-  let isKugouReg = new RegExp("^[非VIP]*点歌*(kugou|酷狗)(.*)|^[非VIP]*#点歌*(kugou|酷狗)(.*)$");
+  let isKugouReg = new RegExp("^[非VIP]*点歌*(kugou|酷狗)(.*)$");
   let isWangYiyunReg = new RegExp("^[非VIP]*点歌*(网易云|网抑云)(.*)$");
   
   let isQQ = isQQReg.test(msg);
@@ -95,18 +61,16 @@ msg3 = "https://www.bavei.com/vip/?url=" + "https:"  + data3
   let isPay = msg.includes("非VIP");
   console.log("什么！这个靓仔点非VIP？？？");
   msg = msg.replace(/[非VIP|点歌|qq|QQ|kugou|酷狗|网易云|网抑云]/g, "");
-  
+  msg = msg.replace(/#/g,"").trim()
   
    if(e.msg.includes("#听")){
 		
 		msg = msg2
+		
            }else{
 	    msg2 = msg
 		   }
-		  msg = msg.replace(/#/g,"")
   console.log("这个靓仔在搜", msg);
-   
-  
   try {
 	 
     msg = encodeURI(msg);
@@ -115,10 +79,11 @@ msg3 = "https://www.bavei.com/vip/?url=" + "https:"  + data3
     let url = urlList[apiName].replace("paramsSearch", msg);
     let response = await fetch(url);
     const { data, result } = await response.json();
+	console.log(data)
+	
     let songList = [];
-    if (isQQ)
-      songList = isPay ? data.song.list.filter((item) => !item.pay.payinfo) : data.song.list;
-    else if (isKugou) songList = isPay ? data.info.filter((item) => !item.pay_type_sq) : data.info;
+    
+    if (isKugou) songList = isPay ? data.info.filter((item) => !item.pay_type_sq) : data.info;
     else songList = result?.songs?.length ? result.songs : [];
 	if(zt == 0){
 		kg = isKugou
@@ -132,11 +97,10 @@ msg3 = "https://www.bavei.com/vip/?url=" + "https:"  + data3
 		  if(e.msg.includes("#听")){
 		
 		id = e.msg.replace(/#听/g,"").trim()
+		
 		console.log("id为"+id)
            }
-    if (!songList[0]) {
-      await e.reply(`没有找到该歌曲哦`);
-    } else if (e.isPrivate) {
+    if (e.isPrivate) {
 		
       await e.friend.shareMusic(
         isQQ ? "qq" : isKugou ? "kugou" : "163",
@@ -144,8 +108,37 @@ msg3 = "https://www.bavei.com/vip/?url=" + "https:"  + data3
       );
     } else if (e.isGroup )  {
 		
-		console.log(isKugou)
+		
 		if(Number(id)>0){
+			console.log(qq)
+			if(qq){
+				let url = urlList[apiName].replace("paramsSearch", msg2) 
+				url = url + String(id)
+				console.log(url)
+                let response2 = await fetch(url);
+                  const data2= await response2.json();
+	                console.log(data2)
+				console.log(data2.data.music)
+				
+			url = "https://y.qq.com/n/ryqq/songDetail/" + data2.data.Mid; //接口地址
+			
+			console.log(url)
+			
+			let response3 = await fetch(url); //调用接口获取数据
+			let  res = await response3.text(); //结果json字符串转对象
+			       
+			let n1 = res.indexOf('","id":') + 7
+			
+			let n2 = res.indexOf(',',n1)
+			console.log(n1,n2)
+			
+			e.group.shareMusic("qq",Number(res.slice(n1,n2)))
+			e.reply(segment.record(data2.data.music))
+			 console.log("结果是：" + res.slice(n1,n2))
+			 qq = 0
+			 zt = 0
+			}
+			
 			 e.group.shareMusic(
         isQQ ? "qq" : isKugou ? "kugou" : "163",
         isQQ ? songList[ id-1 ].id : isKugou ? songList[ id-1 ].hash : songList[ id-1 ].id
@@ -158,13 +151,31 @@ msg3 = "https://www.bavei.com/vip/?url=" + "https:"  + data3
 	  let msg = ""
 	  let zz = ""
 	  
+	  
+	  if(isQQ &id == ""){
+		  for(let i=0;i<data.length;i++){
+
+				  msg = msg + "\n"+  String(i+1)+ ".  "+data[i]. song + "   作者："+ data[i]. singers
+				  
+	  }
+		ForwardMsg(e,[msg + "\n请发送你要听的序列号的歌曲，例如 #听1"])
+			       
+			
+			 console.log("结果是：" + data[0]. song + data[0]. singers)
+		 zt = 1
+		 
+	}
+	  
+	  
+	  
+	  
 	  if(isKugou &id == ""){
 		  for(let i=0;i<data. info.length;i++){
 
 				  msg = msg + "\n"+  String(i+1)+ ".  "+data. info[i].songname + "   作者："+ data. info[i].singername
 				  
 	  }
-	  console.log(msg)
+	
 	   ForwardMsg(e,[msg + "\n请发送你要听的序列号的歌曲，例如 #听1"])
 	   zt = 1
 	  }
@@ -178,10 +189,10 @@ msg3 = "https://www.bavei.com/vip/?url=" + "https:"  + data3
 	  
 		  }
 		  
-		  console.log(msg)
+		  
 		  ForwardMsg(e,[msg + "\n请发送你要听的序列号的歌曲，例如 #听1"])
 		  zt = 1
-		  console.log(songList[0].artists[0])
+		  
 		  
  
       }
