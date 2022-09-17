47605a087c25c4c5d51bf2e91c78c07d73421e56
越追越远 <2859167710@qq.com>
2022-09-17 05:57:37 +0000

    update apps/geterror.js. reg: "^#lin(发送|获取)?日志$"reg: "^#lin(删除|重置|刷新|备份)?报错$"reg: "^#lin(发送|获取)?报错$"
    Signed-off-by: 越追越远 <2859167710@qq.com>

--- apps/geterror.js
+++ apps/geterror.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs'
 import { segment } from 'oicq'
 const errorpath = `./logs/error.log`;
-
+const date = new Date();
 export class geterror extends plugin {
     constructor() {
         super({
@@ -21,9 +21,15 @@ export class geterror extends plugin {
                     fnc: 'geterror'
                 }, {
                     /** 命令正则匹配 */
-                    reg: "^#lin(重置|删除)报错$", //匹配消息正则，命令正则
+                    reg: "^#lin(删除|重置|刷新|备份)?报错$", //匹配消息正则，命令正则
                     /** 执行方法 */
                     fnc: 'errorback'
+                },
+                {
+                    /** 命令正则匹配 */
+                    reg: "^#lin(发送|获取)?日志$", //匹配消息正则，命令正则
+                    /** 执行方法 */
+                    fnc: 'getjournal'
                 }
             ]
         })
@@ -48,6 +54,23 @@ export class geterror extends plugin {
             e.friend.sendFile(errorpath)
         }
     }
+    async getjournal(e) {
+        if (!e.isMaster) {
+            e.reply([segment.at(e.user_id), `\n凡人，休得僭越！`]);
+            return true
+        }
+        if (e.isGroup) {
+            e.reply("请主人私聊我哦！")
+            return true;
+        }
+        let journalpath = `./logs/command.${date.getFullYear}` + `-${date.getMonth}-` + `${date.getDate}.log`
+        if (!fs.existsSync(journalpath)) {
+            e.reply(`${journalpath}不存在。`)
+        }
+        else {
+            e.friend.sendFile(journalpath)
+        }
+    }
     async errorback(e) {
         if (!e.isMaster) {
             e.reply([segment.at(e.user_id), `\n凡人，休得僭越！`]);
@@ -61,16 +84,15 @@ export class geterror extends plugin {
             e.reply(`${errorpath}不存在报错文件。`)
         }
         else {
-            const date = new Date();
             let errorbackpath = `./logs/error.` + date + `.back.log`
             fs.copyFileSync(`${errorpath}`, `${errorbackpath}`);
+            e.reply(`${configyamlpath}存在配置，已经自动重置并备份。`)
             fs.unlink(errorpath, function (err) {
                 if (err) {
                     throw err;
                 }
                 console.log('文件:' + errorpath + '删除成功！');
             })
-            e.reply(`${configyamlpath}存在配置，已经自动重置并备份。`)
         }
     }
 }
