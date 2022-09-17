e4d3350e1d294e86b5345c945e3dccf13f72f31d
越追越远 <2859167710@qq.com>
2022-09-17 09:35:11 +0000

    update apps/help.js.
    Signed-off-by: 越追越远 <2859167710@qq.com>

--- apps/help.js
+++ apps/help.js
@@ -1,18 +1,10 @@
 import plugin from '../../../lib/plugins/plugin.js'
-import cfg from '../../../lib/config/config.js'
 import { createRequire } from "module";
-//项目路径
-//如果报错请删除Yunzai/data/目录中lin文件夹
-const dirpath = "data/lin/";//文件夹路径
-var filename = `battle`;//文件名
-if (filename.indexOf(".json") == -1) {//如果文件名不包含.json
-	filename = filename + ".json";//添加.json
-}
+import puppeteer from "../../..//lib/puppeteer/puppeteer.js";
+import command from '../command/command.js'
+var msg = await command.getresources("help", "help");
 const require = createRequire(import.meta.url);
 const { exec, execSync } = require("child_process");
-const helppath = `./plugins/lin/resources/lin帮助.txt`;
-const _path = process.cwd();
-//配置一些有意思的参数
 
 export class help extends plugin {
 	constructor() {
@@ -35,36 +27,23 @@ export class help extends plugin {
 			]
 		})
 	}
+
 	/**
 	 * 
 	 * @param e oicq传递的事件参数e
 	 */
 	async rules(e) {
-		if (e.isPrivate
-		) { e.friend.sendFile(helppath) }
-		else {
-			e.group.fs.upload(helppath)
+		let data1 = {}
+		let ml = process.cwd()
+		console.log(ml)
+		data1 = {
+			tplFile: './plugins/lin/resources/html2/2.html',
+			cs: msg,
+			dz: ml
 		}
-		//let msg1 = "发现新版本，自动更新！"
-		for (let i of cfg.masterQQ) { //这里定义发送给所有主人
-			let userId = i
-			//Bot.pickUser(userId).sendMsg(msg1)
-		}
-		let command = "git pull";
-		//下面是强制更新，如果需要可以替换上面这句！！！！！！！！！！！！！
-		//let command = "git checkout . && git pull";
-		var ls = exec(command, { cwd: `${_path}/plugins/lin/` }, async function (error, stdout, stderr) {
-			let isChanges = error.toString().includes("Your local changes to the following files would be overwritten by merge") ? true : false;
-
-			let isNetwork = error.toString().includes("fatal: unable to access") ? true : false;
-
-			if (isChanges) {
-				let msg2 = "新版本自动更新失败，请手动更新！ "
-				for (let i of cfg.masterQQ) { //这里定义发送给所有主人
-					let userId = i
-					Bot.pickUser(userId).sendMsg(msg2)
-				}
-			}
+		let img = await puppeteer.screenshot("123", {
+			...data1,
 		});
+		e.reply(img)
 	}
 }
