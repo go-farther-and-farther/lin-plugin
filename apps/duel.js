ef7c3620a15ef74400db3502c6a652658c0303b0
越追越远 <2859167710@qq.com>
2022-09-25 08:08:29 +0000

    update apps/duel.js.
    Signed-off-by: 越追越远 <2859167710@qq.com>

--- apps/duel.js
+++ apps/duel.js
@@ -133,56 +133,44 @@ export class duel extends plugin {//决斗
 		}, Cooling_time * 1000);
 		//计算实时内力的影响,等级在1-13级之间
 		//  随机加成部分    +      境界加成部分 * 内力 * 随机发挥效果 //最大内力差为18*1.5*energy
-		let i = Math.random() * 100 * Magnification
-		let i_2 = (0.15 * level + 1) * energy*Magnification2
-		let j = Math.random() * 100 * Magnification
-		let j_2 = (0.15 * level2 + 1) * energy2*Magnification2
+		let i = Math.random() * 10 * Magnification
+		let i_2 = level * Magnification2
+		let j = Math.random() * 10 * Magnification
+		let j_2 = level2 * Magnification2
+		let k = Math.round(i + i_2 - j - j_2)//取整数
 		i = Math.round(i)
 		i_2 = Math.round(i_2)
 		j = Math.round(j)
 		j_2 = Math.round(j_2)
-		let k = Math.round((i + i_2 - j - j_2) / 60)//取整数
 		e.reply([segment.at(e.user_id),
 		`\n你的境界为${json[user_id].levels}\n${user_id2_nickname}的境界是${json[user_id2].levels}\n决斗开始！天时地利随机系数${Magnification}`]);//发送消息
 		if (json[user_id2].Privilege == 1 || e.sender.role == "owner" || e.sender.role == "admin") {
-			json[user_id].energy -= 3
-			if (k <= 0)
-				k = -k + 1
-			setTimeout(() => {//延迟5秒
-				e.group.muteMember(user_id2, (k) * 60); //禁言
-				e.reply([segment.at(e.user_id),
-				`你获得天时地利随机加成${i},综合实力${i_2},\n${user_id2_nickname}天时地利随机加成${j},综合内力${j_2}\n你使用了管理员（半步）之力获得了胜利，恭喜你与${user_id2_nickname}决斗成功。\n${user_id2_nickname}接受惩罚，已被禁言${k}分钟！\n你的内力-3`]);//发送消息
-			}, 5000);//设置延时
+			setTimeout(() => {//延迟3秒
+				e.group.muteMember(user_id2, 60); //禁言
+				e.reply([segment.at(e.user_id), `你不讲武德，使用了管理员之力获得了胜利。\n恭喜你与${user_id2_nickname}决斗成功。\n${user_id2_nickname}接受惩罚，已被禁言1分钟！\n你的内力-3`]);//发送消息
+			}, 3000);//设置延时
 		}
 		else if (json[user_id].Privilege == 1 || e.group.pickMember(e.at).is_owner || e.group.pickMember(e.at).is_admin) {
-			k = -k + 1
-			json[user_id].energy--
-			json[user_id2].energy -= 2
 			setTimeout(() => {
-				e.group.muteMember(user_id, (k) * 60); //禁言
-				e.reply([segment.at(e.user_id), `你获得天时地利随机加成${i},综合实力${i_2},\n${user_id2_nickname}天时地利随机加成${j},综合内力${j_2}\n对方使用了管理员（半步）之力，你与${user_id2_nickname}决斗失败。\n你接受惩罚，已被禁言${k + 1}分钟！\n你的内力-1，${user_id2_nickname}内力-2`]);//发送消息
-			}, 5000);//设置延时
+				e.group.muteMember(user_id, 60); //禁言
+				e.reply([segment.at(e.user_id), `对方不讲武德，使用了管理员之力获得了胜利。\n你接受惩罚，已被禁言1分钟！\n你的内力-1。`]);//发送消息
+			}, 3000);//设置延时
 		}
 		else if (k > 0) {//判断是否成功
-			json[user_id].energy -= 3
 			setTimeout(() => {//延迟5秒
 				e.group.muteMember(user_id2, (k) * 60); //禁言
 				e.reply([segment.at(e.user_id),
-				`你获得天时地利随机加成${i},综合实力${i_2},\n${user_id2_nickname}天时地利随机加成${j},综合内力${j_2}\n恭喜你与${user_id2_nickname}决斗成功。\n${user_id2_nickname}接受惩罚，已被禁言${k}分钟！\n你的内力-3`]);//发送消息
-			}, 5000);//设置延时
+				`你获得天时地利随机加成${i},综合实力${i_2},\n${user_id2_nickname}天时地利随机加成${j},综合内力${j_2}\n恭喜你与${user_id2_nickname}决斗成功。\n${user_id2_nickname}接受惩罚，已被禁言${k}分钟！`]);//发送消息
+			}, 3000);//设置延时
 		}
 		else {
 			k = -k + 1
-			json[user_id].energy--
-			json[user_id2].energy -= 2
 			setTimeout(() => {
 				e.group.muteMember(user_id, (k) * 60); //禁言
-				e.reply([segment.at(e.user_id), `你获得天时地利随机加成${i},综合实力${i_2},\n${user_id2_nickname}天时地利随机加成${j},综合内力${j_2}\n你与${user_id2_nickname}决斗失败。\n你接受惩罚，已被禁言${k + 1}分钟！\n你的内力-1，${user_id2_nickname}内力-2`]);//发送消息
-			}, 5000);//设置延时
+				e.reply([segment.at(e.user_id), `你获得天时地利随机加成${i},综合实力${i_2},\n${user_id2_nickname}天时地利随机加成${j},综合内力${j_2}\n你与${user_id2_nickname}决斗失败。\n你接受惩罚，已被禁言${k + 1}分钟！`]);//发送消息
+			}, 3000);//设置延时
 		}//内力小于0时候重置内力
-		if (json[user_id].energy < 0) { json[user_id].energy = 0 }
-		if (json[user_id2].energy < 0) { json[user_id2].energy = 0 }
-		console.log(`发起者：${user_id}被动者： ${user_id2}随机时间：${i}秒钟`); //输出日志
+		console.log(`发起者：${user_id}被动者： ${user_id2}随机时间：${k}分钟`); //输出日志
 		fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(json, null, "\t"));//写入文件
 		return true; //返回true 阻挡消息不再往下}
 	}
