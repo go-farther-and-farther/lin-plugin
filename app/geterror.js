d8253f429663ac5440d9c54dc9b9be3898a99366
越追越远 <2859167710@qq.com>
2022-09-27 04:13:55 +0000

    改名lin-plugin
    Signed-off-by: 越追越远 <2859167710@qq.com>

---
+++ app/geterror.js
@@ -0,0 +1,99 @@
+import fs from 'node:fs'
+import { segment } from 'oicq'
+const errorpath = `./logs/error.log`;
+export class geterror extends plugin {
+    constructor() {
+        super({
+            /** 功能名称 */
+            name: '发送错误日志',
+            /** 功能描述 */
+            dsc: '',
+            /** https://oicqjs.github.io/oicq/#events */
+            event: 'message',
+            /** 优先级，数字越小等级越高 */
+            priority: 1000,
+            rule: [
+                {
+                    /** 命令正则匹配 */
+                    reg: "^#lin(发送|获取)?报错$", //匹配消息正则，命令正则
+                    /** 执行方法 */
+                    fnc: 'geterror'
+                }, {
+                    /** 命令正则匹配 */
+                    reg: "^#lin(删除|重置|刷新|备份)?报错$", //匹配消息正则，命令正则
+                    /** 执行方法 */
+                    fnc: 'errorback'
+                },
+                {
+                    /** 命令正则匹配 */
+                    reg: "^#lin(发送|获取)?日志$", //匹配消息正则，命令正则
+                    /** 执行方法 */
+                    fnc: 'getjournal'
+                }
+            ]
+        })
+    }
+    /**
+     * 
+     * @param e oicq传递的事件参数e
+     */
+    async geterror(e) {
+        if (!e.isMaster) {
+            e.reply([segment.at(e.user_id), `\n凡人，休得僭越！`]);
+            return true
+        }
+         if (!fs.existsSync(errorpath)) {
+            e.reply(`${errorpath}不存在。`)
+        }
+        if (e.isGroup) {
+            e.group.fs.upload(errorpath)
+            return true;
+        }
+        if (e.isPrivate) {
+            e.friend.sendFile(errorpath)
+            return true;
+        }
+    }
+    async getjournal(e) {
+        if (!e.isMaster) {
+            e.reply([segment.at(e.user_id), `\n凡人，休得僭越！`]);
+            return true
+        }
+        var date = new Date();
+        let month = date.getMonth() + 1
+        let journalpath = `./logs/command.${date.getFullYear()}-${month}-${date.getDate()}.log`
+        if (!fs.existsSync(journalpath)) {
+            e.reply(`${journalpath}不存在。`)
+        }
+        if (e.isGroup) {
+            e.group.fs.upload(journalpath)
+            return true;
+        }
+        if (e.isPrivate) {
+            e.friend.sendFile(journalpath)
+            return true;
+        }
+    }
+    async errorback(e) {
+        if (!e.isMaster) {
+            e.reply([segment.at(e.user_id), `\n凡人，休得僭越！`]);
+            return true
+        }
+        if (!fs.existsSync(errorpath)) {
+            e.reply(`${errorpath}不存在报错文件。`)
+        }
+        else {
+            var date = new Date();
+            let month = date.getMonth() + 1
+            let errorbackpath = `./logs/error.${date.getFullYear()}-${month}-${date.getDate()}back.log`
+            fs.copyFileSync(`${errorpath}`, `${errorbackpath}`);
+            e.reply(`${errorpath}存在报错，已经自动重置并备份。`)
+            fs.unlink(errorpath, function (err) {
+                if (err) {
+                    throw err;
+                }
+                console.log('文件:' + errorpath + '删除成功！');
+            })
+        }
+    }
+}
---
+++ app/thumbUp.js
@@ -0,0 +1,107 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import schedule from "node-schedule";
+import command from '../command/command.js'
+var everyone = await command.getConfig("thumbUp_cfg", "everyone") //是否全局点赞
+var reply_something = await command.getConfig("thumbUp_cfg", "reply_something") //是否有点赞提示
+var delayed = await command.getConfig("thumbUp_cfg", "time") * 1000;//这个是间隔时间
+var huifu = await command.getConfig("thumbUp_cfg", "huifu");//这个是点赞完成后推送消息的概率
+var url = '';//这个是接口,获取图片的。
+let id = [];//这个是点赞名单,空则全部点赞
+let blacklist = [];//这个是不发送提示消息的黑名单，有的人怕被骚扰。
+let blacklist_id = [];//这个是黑名单id
+let words = ['早上好！', "你的喜欢是对我最大的支持！", "早上好哦！"]//这个是点赞完之后说的话
+var alllist = Bot.fl//获取全部好友名单
+idlist = [];
+for (var key of alllist) {
+	idlist.push(key[0])
+}
+//判断白名单模式还是全局模式，想要名单为空并且配置开启全局点赞
+if (!(id.length == 0 && everyone == 1)) {
+	var idlist = id;
+}
+export class thumbUp extends plugin {
+	constructor() {
+		super({
+			/** 功能名称 */
+			name: '点赞',
+			/** 功能描述 */
+			dsc: '发送点赞或者到点点赞',
+			/** https://oicqjs.github.io/oicq/#events */
+			event: 'message',
+			/** 优先级，数字越小等级越高 */
+			priority: 1000,
+			rule: [
+				{
+					/** 命令正则匹配 */
+					reg: "^#(发起|开始)?(点赞|打卡)(.*)$", //匹配消息正则，命令正则
+					/** 执行方法 */
+					fnc: 'thumbUp'
+				}
+			]
+		})
+	}
+	/**
+	 * 
+	 * @param e oicq传递的事件参数e
+	 */
+	async thumbUp(e) {
+		e.reply(`开始任务！`)
+		console.log(`开始点赞，点赞名单：`, idlist, `正在点赞中...`)
+		for (let i = 0; i < idlist.length; i++) {
+			setTimeout(() => {
+				console.log(`本次为第${i}次点赞，正在点赞中...`)
+				if (!blacklist_id.includes(idlist[i])) {
+					//判断是否在黑名单中，在则跳过
+					Bot.pickFriend(idlist[i]).thumbUp(10);//点赞10次，默认没有svip
+					let l = Math.round(Math.random() * 100)//获取一个0~100的随机数
+					if (!blacklist.includes(id[i]) && l < huifu && reply_something == 1) {//这里是消息的触发概率
+						let msg = []
+						if (url == '') {
+							msg = [
+								words[Math.floor(Math.random() * words.length)],
+							];
+						}
+						else {
+							msg = [
+								words[Math.floor(Math.random() * words.length)],
+								segment.image(url),
+							];
+						}
+						Bot.pickUser(idlist[i]).sendMsg(msg)
+					}
+				}
+			}, delayed * i);//设置延时
+		}
+	}
+}
+let time_ = String(Math.floor(Math.random() * 60)) + ' ' + String(Math.floor(Math.random() * 60)) + ' ' + String(Math.floor(Math.random() * 2) + 6) + ' * * *'
+//这个是获取一个6~7点的时间，到了时间则执行任务
+schedule.scheduleJob(time_, function () {
+	console.log(`开始点赞，点赞名单：`, idlist, `正在点赞中...`)
+	for (let i = 0; i < idlist.length; i++) {
+		setTimeout(() => {
+			console.log(`本次为第${i}次点赞，正在点赞中...`)
+			if (!blacklist_id.includes(idlist[i])) {
+				//判断是否在黑名单中，在则跳过
+				Bot.pickFriend(idlist[i]).thumbUp(10);//点赞10次，默认没有svip
+				let l = Math.round(Math.random() * 100)//获取一个0~100的随机数
+				if (!blacklist.includes(id[i]) && l < huifu && reply_something == 1) {//这里是消息的触发概率
+					let msg = []
+					if (url == '') {
+						msg = [
+							words[Math.floor(Math.random() * words.length)],
+						];
+					}
+					else {
+						msg = [
+							words[Math.floor(Math.random() * words.length)],
+							segment.image(url),
+						];
+					}
+					Bot.pickUser(idlist[i]).sendMsg(msg)
+				}
+			}
+		}, delayed * i);//设置延时
+	}
+}
+);
