1d918293b715d16954fb34869034829a145e844a
go-farther-and-farther <2859167710@qq.com>
2022-10-16 18:01:38 +0800

    modified:   config/system/help_system.js

--- config/system/help_system.js
+++ config/system/help_system.js
@@ -1,185 +1,215 @@
 /*
-* 请注意，系统不会读取help_default.js ！！！！
-* 【请勿直接修改此文件，且可能导致后续冲突】
+* 此配置文件为系统使用，请勿修改，否则可能无法正常使用
 *
-* 如需自定义可将文件【复制】一份，并重命名为 help.js
+* 如需自定义配置请复制修改上一级help_default.js
 *
 * */
 
-// 帮助配置
 export const helpCfg = {
-  // 帮助标题
-  title: 'Lin帮助',
-
-  // 帮助副标题
-  subTitle: 'Yunzai-Bot & Lin-Plugin',
-
-  // 帮助表格列数，可选：2-5，默认3
-  // 注意：设置列数过多可能导致阅读困难，请参考实际效果进行设置
-  colCount: 3,
-
-  // 单列宽度，默认265
-  // 注意：过窄可能导致文字有较多换行，请根据实际帮助项设定
+  title: '麟帮助',
+  subTitle: 'Yunzai-Bot & lin-plugin',
+  columnCount: 3,
   colWidth: 265,
-
-  // 皮肤选择，可多选，或设置为all
-  // 皮肤包放置于 resources/help/theme
-  // 皮肤名为对应文件夹名
-  // theme: 'all', // 设置为全部皮肤
-  // theme: ['default','theme2'], // 设置为指定皮肤
   theme: 'all',
-
-  // 排除皮肤：在存在其他皮肤时会忽略该项内设置的皮肤
-  // 默认忽略default：即存在其他皮肤时会忽略自带的default皮肤
-  // 如希望default皮肤也加入随机池可删除default项
   themeExclude: ['default'],
-
-  // 是否启用背景毛玻璃效果，若渲染遇到问题可设置为false关闭
-  bgBlur: true
+  style: {
+    fontColor: '#ceb78b',
+    descColor: '#eee',
+    contBgColor: 'rgba(6, 21, 31, .5)',
+    contBgBlur: 3,
+    headerBgColor: 'rgba(6, 21, 31, .4)',
+    rowBgColor1: 'rgba(6, 21, 31, .2)',
+    rowBgColor2: 'rgba(6, 21, 31, .35)'
+  }
 }
 
-// 帮助菜单内容
 export const helpList = [{
-  group: '娱乐功能',
-  list: [{
-    icon: 80,
-    title: '#摸鱼日记',
-    desc: '诶嘿。'
-  }, {
-    icon: 46,
-    title: '#我在人间凑数的日子',
-    desc: '人终究还是要活着'
-  }, {
-    icon: 33,
-    title: '#舔狗日记',
-    desc: '其实当舔狗也不容易......'
-  }, {
-    icon: 31,
-    title: '#生成签名xx',
-    desc: '随机生成签名'
-  }, {
-    icon: 22,
-    title: '#举牌xx',
-    desc: '打call'
-  }, {
-    icon: 52,
-    title: '#骂我',
-    desc: '承受能力弱的请勿使用此指令'
-  }, {
-    icon: 57,
-    title: '#来句诗',
-    desc: '今日份诗句，请查收'
+  "group": "Bot相关",
+  "list": [{
+    "icon": 1,
+    "title": "#发好友 <QQ> <消息>", "desc": "给好友发送一条涩涩的消息"
+  },
+  {
+    "icon": 2,
+    "title": "#发群聊 <群号> <消息>",
+    "desc": "给群聊发送一条涩涩的消息"
+  },
+  {
+    "icon": 3,
+    "title": "#改头像 <图片>",
+    "desc": "顾名思义"
+  },
+  {
+    "icon": 4,
+    "title": "#改状态 <状态> ",
+    "desc": "顾名思义"
+  },
+  {
+    "icon": 5, "title": "#改昵称 <昵称> ",
+    "desc": "顾名思义"
+  },
+  {
+    "icon": 6, "title": "#改签名 <签名> ",
+    "desc": "顾名思义"
+  },
+  {
+    "title": "#改性别 <性别> ",
+    "desc": "顾名思义",
+    "icon": 7
+  },
+  {
+    "title": "#改群名片 <名片> ",
+    "desc": "群里Bot自己的名片",
+    "icon": 8
+  },
+  {
+    "title": "#改群昵称 <昵称>",
+    "desc": "改群的昵称", "icon": 9
+  },
+  {
+    "title": "#改群头像 <图片>",
+    "desc": "顾名思义",
+    "icon": 10
+  },
+  {
+    "title": "#删好友 <QQ> ",
+    "desc": "删掉涩涩的好友",
+    "icon": 11
+  },
+  {
+    "title": "#退群 <群号> ",
+    "desc": "退掉涩涩的群",
+    "icon": 12
+  },
+  {
+    "title": "#获取群列表",
+    "desc": "获取Bot的所有群",
+    "icon": 13
+  },
+  {
+    "title": "#获取好友列表",
+    "desc": "获取Bot的所有好友",
+    "icon": 14
+  },
+  {
+    "title": "#取说说列表 <页数> ",
+    "desc": "获取Bot的说说列表",
+    "icon": 15
+  },
+  {
+    "title": "#发说说 <内容> ",
+    "desc": "发送一条涩涩的说说",
+    "icon": 16
+  },
+  {
+    "title": "#删说说 <序号>",
+    "desc": "用取说说列表获取序号",
+    "icon": 17
+  },
+  {
+    "title": "#清空说说",
+    "desc": "一键清空",
+    "icon": 18
+  },
+  {
+    "title": "#清空留言",
+    "desc": "一键清空留言",
+    "icon": 19
   }]
-}, {
-  group: "查询指令",
-  list: [{
-    icon: 58,
-    title: '#来份动漫图',
-    desc: '随机发送动漫图片'
+},
+{
+  "group": "通知相关",
+  "list": [{
+    "icon": 5,
+    "title": "#麟设置 xx 开启/关闭",
+    "desc": "具体可使用#麟设置查看"
+  },
+  {
+    "icon": 12,
+    "title": "#麟启用全部通知",
+    "desc": "一键启用全部通知"
+  },
+  {
+    "icon": 20,
+    "title": "#麟禁用全部通知",
+    "desc": "一键禁用全部通知"
+  },
+  ]
+},
+{
+  "group": "娱乐功能",
+  "list": [{
+    "icon": 20,
+    "title": "#唱歌",
+    "desc": "随机唱鸭"
+  },
+  {
+    "icon": 13,
+    "title": "#赞我",
+    "desc": "给你点一个大大的赞"
+  },
+  {
+    "icon": 7,
+    "title": "#椰羊收益曲线",
+    "desc": "查看角色收益曲线"
+  },
+  {
+    "icon": 9,
+    "title": "#收益曲线帮助",
+    "desc": "=-="
   }, {
-    icon: 43,
-    title: '#搜歌xx #搜动漫xx',
-    desc: '功能请看指令'
+    "icon": 15,
+    "title": "#支付宝到账<数字>",
+    "desc": "听到账爽一下"
   }, {
-    icon: 59,
-    title: '#历史上的今天',
-    desc: '了解历史上的今天发生的事情'
-  }, {
-    icon: 21,
-    title: '#知乎热榜',
-    desc: '发送知乎热榜'
-  }, {
-    icon: 39,
-    title: '#微博热搜',
-    desc: '发送当前微博热搜'
-  }, {
-    icon: 55,
-    title: '#网易云热评',
-    desc: '发送网易云上的热评'
-  }, {
-    icon: 52,
-    title: '#翻译xx',
-    desc: '只是翻译而已'
-  }, {
-    icon: 76,
-    title: '#王者xx语音',
-    desc: '发送王者里xx的语音'
-  }, {
-    icon: 78,
-    title: '#查询手机号xx',
-    desc: '了解手机号的信息'
-  }, {
-    icon: 79,
-    title: '#地点查询xx',
-    desc: '查询某一地方的信息'
-  }, {
-    icon: 60,
-    title: '#路线规划查询xx到xx',
-    desc: '帮助规划路线'
+    "icon": 17,
+    "title": "#翻译",
+    "desc": "有道翻译"
   }]
-}, {
-  group: '决斗小游戏',
-  list: [{
-    icon: 21,
-    title: '#游戏帮助',
-    desc: '获取决斗游戏帮助'
+},
+{
+  "group": "其他功能",
+  "list": [{
+    "icon": 7,
+    "title": "#取直链 <图片>",
+    "desc": "取图片的链接"
+  }, {
+    "icon": 20,
+    "title": "#取face <face表情>",
+    "desc": "没啥用=-="
+  }, {
+    "icon": 1,
+    "title": "#群星级",
+    "desc": "查看群星级"
   }]
-}, {
-  group: "其他功能",
-  list: [{
-    icon: 38,
-    title: '#Lin文案帮助',
-    desc: '查询文案帮助'
-  }]
-}, {
-  group: '管理命令，仅管理员可用',
-  auth: 'master',
-  list: [{
-    icon: 95,
-    title: '#Lin更新',
-    desc: '更新Lin插件'
+},
+{
+  "group": "管理命令，仅管理员可用",
+  "auth": "master",
+  "list": [{
+    "icon": 8,
+    "title": "#麟设置",
+    "desc": "查看麟设置"
+  },
+  {
+    "icon": 1,
+    "title": "#麟(强制)更新",
+    "desc": "更新麟"
+  },
+  {
+    "icon": 15,
+    "title": "#麟版本",
+    "desc": "查看版本信息"
+  },
+  {
+    "icon": 12,
+    "title": "#麟更新日志",
+    "desc": "查看更新日志"
   }, {
-    icon: 03,
-    title: '#Lin版本',
-    desc: '查看版本记录'
-  }, {
-    icon: 11,
-    title: '#跑路 #回来',
-    desc: '是否回复本群消息'
-  }, {
-    icon: 80,
-    title: 'ai开启/关闭',
-    desc: 'ai开关'
-  }, {
-    icon: 01,
-    title: '太吵了/太安静了',
-    desc: 'ai触发概率调整'
-  }, {
-    icon: 02,
-    title: '只关注@消息/关注所有消息',
-    desc: 'ai模式调整'
-  }, {
-    icon: 22,
-    title: '#Lin发送报错',
-    desc: '获取Lin报错日志'
-  }, {
-    icon: 22,
-    title: '#Lin重置报错',
-    desc: 'Lin重置报错消息'
-  }, {
-    icon: 71,
-    title: '#Lin发送日志',
-    desc: '获取Lin运行日志'
-  }, {
-    icon: 74,
-    title: '发送Lin配置',
-    desc: '管理功能'
-  }, {
-    icon: 54,
-    title: '#重置Lim配置',
-    desc: '管理功能'
+    "icon": 8,
+    "title": "#麟状态",
+    "desc": "查看系统状态"
   }]
 }]
-
 
 export const isSys = true
