99777ab6a078137ed7d023439897828720d69dd8
go-farther-and-farther <2859167710@qq.com>
2022-11-14 19:39:31 +0800

    modified:   README.md 	modified:   apps/Update.js

--- apps/Update.js
+++ apps/Update.js
@@ -1,311 +1,132 @@
-import plugin from "../../../lib/plugins/plugin.js";
+import plugin from '../../../lib/plugins/plugin.js'
+import fetch from 'node-fetch'
 import { createRequire } from "module";
-import lodash from "lodash";
-import { Restart } from "../../other/restart.js";
-import puppeteer from "../../../lib/puppeteer/puppeteer.js";
 
 const require = createRequire(import.meta.url);
 const { exec, execSync } = require("child_process");
 
-// 是否在更新中
-let uping = true;
+const _path = process.cwd();
 
-/**
- * 处理插件更新
- */
 export class update extends plugin {
-  constructor() {
-    super({
-      name: "更新插件",
-      dsc: "更新插件代码",
-      event: "message",
-      priority: 4000,
-      rule: [
-        {
-          reg: "^#*lin(插件)?(强制)?更新",
-          fnc: "update",
-        },
-        {
-          reg: "^#*lin(插件)?版本$",
-          fnc: "version",
-        },
-      ],
-    });
-
-  }
-
-  /**
-   * rule - 更新lin插件
-   * @returns
-   */
-  async update() {
-    if (!this.e.isMaster) return false;
-
-    /** 检查是否正在更新中 */
-    if (uping) {
-      await this.reply("已有命令更新中..请勿重复操作");
-      return;
+    constructor() {
+        super({
+            /** 功能名称 */
+            name: 'lin更新',
+            /** 功能描述 */
+            dsc: 'lin更新自身',
+            /** https://oicqjs.github.io/oicq/#events */
+            event: 'message',
+            /** 优先级，数字越小等级越高 */
+            priority: 999,
+            rule: [
+                {
+                    /** 命令正则匹配 */
+                    reg: '^#(lin更新|lin强制更新)$',
+                    /** 执行方法 */
+                    fnc: 'update'
+                },
+                {
+                    /** 命令正则匹配 */
+                    reg: '^#重启$',
+                    /** 执行方法 */
+                    fnc: 'restartApp'
+                }
+            ]
+        })
     }
 
-    /** 检查git安装 */
-    if (!(await this.checkGit())) return;
+    /**
+     * 
+     * @param e oicq传递的事件参数e
+     */
+    async update(e) {
+        if (!this.e.isMaster) {
+            await this.e.reply("您无权操作");
+            return true;
+        }
 
-    const isForce = this.e.msg.includes("强制");
+        let isForce = this.e.msg.includes("强制") ? true : false;
 
-    /** 执行更新 */
-    await this.runUpdate(isForce);
+        let command = "git pull";
 
-    /** 是否需要重启 */
-    if (this.isUp) {
-      setTimeout(() => this.restart(), 2000);
+        if (isForce) {
+            command = "git checkout . && git pull";
+            await this.e.reply("正在执行强制更新操作，请稍等");
+        } else {
+            await this.e.reply("正在执行更新操作，请稍等");
+        }
+        var me = this;
+        var ls = exec(command, { cwd: `${_path}/plugins/lin-plugin/` }, async function (error, stdout, stderr) {
+            if (error) {
+                let isChanges = error.toString().includes("Your local changes to the following files would be overwritten by merge") ? true : false;
+
+                let isNetwork = error.toString().includes("fatal: unable to access") ? true : false;
+
+                if (isChanges) {
+                    //git stash && git pull && git stash pop stash@{0}
+                    //需要设置email和username，暂不做处理
+                    await me.e.reply(
+                        "失败！\nError code: " +
+                        error.code +
+                        "\n" +
+                        error.stack +
+                        "\n\n本地代码与远程代码存在冲突,上面报错信息中包含冲突文件名称及路径，请尝试处理冲突\n如果不想保存本地修改请使用【#强制更新】\n(注意：强制更新命令会忽略所有本地对lin-plugin插件本身文件的修改，本地修改均不会保存，请注意备份)"
+                    );
+                } else if (isNetwork) {
+                    await me.e.reply(
+                        "失败！\nError code: " + error.code + "\n" + error.stack + "\n\n可能是网络问题，请关闭加速器之类的网络工具，或请过一会尝试。"
+                    );
+                } else {
+                    await me.e.reply("失败！\nError code: " + error.code + "\n" + error.stack + "\n\n出错了。请尝试处理错误");
+                }
+            } else {
+                if (/Already up to date/.test(stdout)) {
+                    e.reply("目前已经是最新了~");
+                    return true;
+                }
+                me.restartApp();
+            }
+        });
+
     }
-  }
+    async restartApp() {
+        if (!this.e.isMaster) {
+            await this.e.reply("您无权操作");
+            return true;
+        }
+        await this.e.reply("开始执行重启，请稍等...");
+        Bot.logger.mark("开始执行重启，请稍等...");
 
+        let data = JSON.stringify({
+            isGroup: this.e.isGroup ? true : false,
+            id: this.e.isGroup ? this.e.group_id : this.e.user_id,
+        });
 
-  /**
-   * 云崽重启操作
-   */
-  restart() {
-    new Restart(this.e).restart();
-  }
+        try {
 
-  /**
-   * lin插件更新函数
-   * @param {boolean} isForce 是否为强制更新
-   * @returns
-   */
-  async runUpdate(isForce) {
-    let command = "git -C ./plugins/lin-plugin/ pull --no-rebase";
-    if (isForce) {
-      command = `git -C ./plugins/lin-plugin/ checkout . && ${command}`;
-      this.e.reply("正在执行强制更新操作，请稍等");
-    } else {
-      this.e.reply("正在执行更新操作，请稍等");
+            await redis.set("Yunzai:lin-plugin:restart", data, { EX: 120 });
+
+            let cm = `npm run start`;
+            if (process.argv[1].includes("pm2")) {
+                cm = `npm run restart`;
+            }
+
+            exec(cm, async (error, stdout, stderr) => {
+                if (error) {
+                    redis.del(`Yunzai:lin-plugin:restart`);
+                    await this.e.reply(`操作失败！\n${error.stack}`);
+                    Bot.logger.error(`重启失败\n${error.stack}`);
+                } else if (stdout) {
+                    Bot.logger.mark("重启成功，运行已转为后台，查看日志请用命令：npm run log");
+                    Bot.logger.mark("停止后台运行命令：npm stop");
+                    process.exit();
+                }
+            });
+        } catch (error) {
+            redis.del(`Yunzai:lin-plugin:restart`);
+            await this.e.reply(`操作失败！\n${error.stack}`);
+        }
+
+        return true;
     }
-    /** 获取上次提交的commitId，用于获取日志时判断新增的更新日志 */
-    this.oldCommitId = await this.getcommitId("lin-plugin");
-    uping = true;
-    let ret = await this.execSync(command);
-    uping = false;
-
-    if (ret.error) {
-      logger.mark(`${this.e.logFnc} 更新失败：lin插件`);
-      this.gitErr(ret.error, ret.stdout);
-      return false;
-    }
-
-    /** 获取插件提交的最新时间 */
-    let time = await this.getTime("lin-plugin");
-
-    if (/(Already up[ -]to[ -]date|已经是最新的)/.test(ret.stdout)) {
-      await this.reply(`lin插件已经是最新版本\n最后更新时间：${time}`);
-    } else {
-      await this.reply(`lin插件\n最后更新时间：${time}`);
-      this.isUp = true;
-      /** 获取lin组件的更新日志 */
-      let log = await this.getLog("lin-plugin");
-      await this.reply(log);
-    }
-
-    logger.mark(`${this.e.logFnc} 最后更新时间：${time}`);
-
-    return true;
-  }
-
-  /**
-   * 获取lin插件的更新日志
-   * @param {string} plugin 插件名称
-   * @returns
-   */
-  async getLog(plugin = "") {
-    let cm = `cd ./plugins/${plugin}/ && git log  -20 --oneline --pretty=format:"%h||[%cd]  %s" --date=format:"%m-%d %H:%M"`;
-
-    let logAll;
-    try {
-      logAll = await execSync(cm, { encoding: "utf-8" });
-    } catch (error) {
-      logger.error(error.toString());
-      this.reply(error.toString());
-    }
-
-    if (!logAll) return false;
-
-    logAll = logAll.split("\n");
-
-    let log = [];
-    for (let str of logAll) {
-      str = str.split("||");
-      if (str[0] == this.oldCommitId) break;
-      if (str[1].includes("Merge branch")) continue;
-      log.push(str[1]);
-    }
-    let line = log.length;
-    log = log.join("\n\n");
-
-    if (log.length <= 0) return "";
-
-    let end = "";
-    end =
-      "更多详细信息，请前往gitee查看\nhttps://gitee.com/go-farther-and-farther/lin-plugin/commits/master";
-
-    log = await this.makeForwardMsg(`lin插件更新日志，共${line}条`, log, end);
-
-    return log;
-  }
-
-  /**
-   * 获取上次提交的commitId
-   * @param {string} plugin 插件名称
-   * @returns
-   */
-  async getcommitId(plugin = "") {
-    let cm = `git -C ./plugins/${plugin}/ rev-parse --short HEAD`;
-
-    let commitId = await execSync(cm, { encoding: "utf-8" });
-    commitId = lodash.trim(commitId);
-
-    return commitId;
-  }
-
-  /**
-   * 获取本次更新插件的最后一次提交时间
-   * @param {string} plugin 插件名称
-   * @returns
-   */
-  async getTime(plugin = "") {
-    let cm = `cd ./plugins/${plugin}/ && git log -1 --oneline --pretty=format:"%cd" --date=format:"%m-%d %H:%M"`;
-
-    let time = "";
-    try {
-      time = await execSync(cm, { encoding: "utf-8" });
-      time = lodash.trim(time);
-    } catch (error) {
-      logger.error(error.toString());
-      time = "获取时间失败";
-    }
-    return time;
-  }
-
-  /**
-   * 制作转发消息
-   * @param {string} title 标题 - 首条消息
-   * @param {string} msg 日志信息
-   * @param {string} end 最后一条信息
-   * @returns
-   */
-  async makeForwardMsg(title, msg, end) {
-    let nickname = Bot.nickname;
-    if (this.e.isGroup) {
-      let info = await Bot.getGroupMemberInfo(this.e.group_id, Bot.uin);
-      nickname = info.card || info.nickname;
-    }
-    let userInfo = {
-      user_id: Bot.uin,
-      nickname,
-    };
-
-    let forwardMsg = [
-      {
-        ...userInfo,
-        message: title,
-      },
-      {
-        ...userInfo,
-        message: msg,
-      },
-    ];
-
-    if (end) {
-      forwardMsg.push({
-        ...userInfo,
-        message: end,
-      });
-    }
-
-    /** 制作转发内容 */
-    if (this.e.isGroup) {
-      forwardMsg = await this.e.group.makeForwardMsg(forwardMsg);
-    } else {
-      forwardMsg = await this.e.friend.makeForwardMsg(forwardMsg);
-    }
-
-    /** 处理描述 */
-    forwardMsg.data = forwardMsg.data
-      .replace(/\n/g, "")
-      .replace(/<title color="#777777" size="26">(.+?)<\/title>/g, "___")
-      .replace(/___+/, `<title color="#777777" size="26">${title}</title>`);
-
-    return forwardMsg;
-  }
-
-  /**
-   * 处理更新失败的相关函数
-   * @param {string} err
-   * @param {string} stdout
-   * @returns
-   */
-  async gitErr(err, stdout) {
-    let msg = "更新失败！";
-    let errMsg = err.toString();
-    stdout = stdout.toString();
-
-    if (errMsg.includes("Timed out")) {
-      let remote = errMsg.match(/'(.+?)'/g)[0].replace(/'/g, "");
-      await this.reply(msg + `\n连接超时：${remote}`);
-      return;
-    }
-
-    if (/Failed to connect|unable to access/g.test(errMsg)) {
-      let remote = errMsg.match(/'(.+?)'/g)[0].replace(/'/g, "");
-      await this.reply(msg + `\n连接失败：${remote}`);
-      return;
-    }
-
-    if (errMsg.includes("be overwritten by merge")) {
-      await this.reply(
-        msg +
-          `存在冲突：\n${errMsg}\n` +
-          "请解决冲突后再更新，或者执行#强制更新，放弃本地修改"
-      );
-      return;
-    }
-
-    if (stdout.includes("CONFLICT")) {
-      await this.reply([
-        msg + "存在冲突\n",
-        errMsg,
-        stdout,
-        "\n请解决冲突后再更新，或者执行#强制更新，放弃本地修改",
-      ]);
-      return;
-    }
-
-    await this.reply([errMsg, stdout]);
-  }
-
-  /**
-   * 异步执行git相关命令
-   * @param {string} cmd git命令
-   * @returns
-   */
-  async execSync(cmd) {
-    return new Promise((resolve, reject) => {
-      exec(cmd, { windowsHide: true }, (error, stdout, stderr) => {
-        resolve({ error, stdout, stderr });
-      });
-    });
-  }
-
-  /**
-   * 检查git是否安装
-   * @returns
-   */
-  async checkGit() {
-    let ret = await execSync("git --version", { encoding: "utf-8" });
-    if (!ret || !ret.includes("git version")) {
-      await this.reply("请先安装git");
-      return false;
-    }
-    return true;
-  }
 }
