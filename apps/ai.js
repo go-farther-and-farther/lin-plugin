c57dce078f16f36d89d8f59ad7d8733c9c8addbd
go-farther-and-farther <2859167710@qq.com>
2022-11-18 20:25:12 +0800

    modified:   apps/ai.js

--- apps/ai.js
+++ apps/ai.js
@@ -64,11 +64,11 @@ export class ai extends plugin {
         console.log("用户命令：", e.msg);
         //一个控制ai回复概率的模块
         let j = Math.random();
-        if (e.isMaster&&e.msg.includes('ai关闭')) { gailv = 0 }
-        else if (e.isMaster&&e.msg.includes('ai开启')) { gailv = 1 }
-        else if (e.isMaster&&e.msg.includes('只关注@信息')) { onlyReplyAt = true; }
-        else if (e.isMaster&&e.msg.includes('关注所有消息')) { onlyReplyAt = false; }       
-        else if (e.isMaster&&e.msg.includes('太安静')) {
+        if ((e.isMaster||e.sender.role == "admin")&&e.msg.includes('ai关闭')) { gailv = 0 }
+        else if ((e.isMaster||e.sender.role == "admin")&&e.msg.includes('ai开启')) { gailv = 1 }
+        else if ((e.isMaster||e.sender.role == "admin")&&e.msg.includes('只关注@信息')) { onlyReplyAt = true; }
+        else if ((e.isMaster||e.sender.role == "admin")&&e.msg.includes('关注所有消息')) { onlyReplyAt = false; }       
+        else if ((e.isMaster||e.sender.role == "admin")&&e.msg.includes('太安静')) {
             //如果概率等于1
             if (gailv > 0.99) {
                 //提示不能修改了
@@ -78,7 +78,7 @@ export class ai extends plugin {
             }
             gailv = gailv + gailv_;
         }
-        else if (e.isMaster&&e.msg.includes('太吵')) {
+        else if ((e.isMaster||e.sender.role == "admin")&&e.msg.includes('太吵')) {
             //如果概率等于0
             if (gailv < 0.01) {
                 //提示不能修改了
