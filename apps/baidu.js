1cedd9c7bdcc0d107374d7e46664c8b78b5f76a5
go-farther-and-farther <2859167710@qq.com>
2022-11-26 15:52:19 +0800

    modified:   apps/baidu.js

--- apps/baidu.js
+++ apps/baidu.js
@@ -20,6 +20,12 @@ export class baidu extends plugin {
                     reg: '#百度(一下)?(.*)',
                     /** 执行方法 */
                     fnc: 'baidu'
+                },
+                {
+                    /** 命令正则匹配 */
+                    reg: '#(.*)疫情',
+                    /** 执行方法 */
+                    fnc: 'yiqing'
                 }
             ]
         })
@@ -28,9 +34,9 @@ export class baidu extends plugin {
      * 
      * @param e oicq传递的事件参数e
      */
+
     async baidu(e) {
         console.log("百度的内容：", e.msg);
-        //接收时将机器人名字替换为青云客AI的菲菲
         if (!e.msg.includes('#百度百科')) {
             let message = e.msg.trim().replace('#百度一下', "").replace(/[\n|\r]/g, "，");//防止把内容里面的一下删了
             message = message.trim().replace('#百度', "").replace(/[\n|\r]/g, "，");
@@ -40,6 +46,46 @@ export class baidu extends plugin {
             let message = e.msg.trim().replace('#百度百科一下', "").replace(/[\n|\r]/g, "，");//防止把内容里面的一下删了
             message = message.trim().replace('#百度百科', "").replace(/[\n|\r]/g, "，");
             var postUrl = `https://baike.baidu.com/search/none?word=${message}`;
+        }
+
+        const puppeteer = require('puppeteer');
+
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--disable-gpu',
+                '--disable-dev-shm-usage',
+                '--disable-setuid-sandbox',
+                '--no-first-run',
+                '--no-sandbox',
+                '--no-zygote',
+                '--single-process'
+            ]
+        });
+        const page = await browser.newPage();
+        await page.goto(postUrl);
+        await page.setViewport({
+            width: 1920,
+            height: 1080
+        });
+
+        await this.reply(segment.image(await page.screenshot({
+            fullPage: true
+        })))
+
+        await browser.close();
+
+    }
+    async yiqing(e) {
+        console.log("疫情的地区：", e.msg);
+        if (e.msg.includes('疫情')) {
+            if(!e.msg.includes('疫情'))
+            {
+                e.reply('请输入“省份-城市疫情”')
+            }
+            let message = e.msg.trim().replace('疫情', "").replace(/[\n|\r]/g, "，");//防止把内容里面的一下删了
+            //message = message.trim().replace('#百度', "").replace(/[\n|\r]/g, "，");
+            var postUrl = `https://voice.baidu.com/act/newpneumonia/newpneumonia/?from=osari_aladin_banner&city=${message}`;
         }
 
         const puppeteer = require('puppeteer');
