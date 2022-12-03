e58240de62f5e5b2528147dc9c6f961f1387caa0
go-farther-and-farther <2859167710@qq.com>
2022-12-03 12:01:44 +0800

    modified:   apps/ai.js 	renamed:    apps/ai2.js -> apps/back/ai.js

--- apps/ai.js
+++ apps/ai.js
@@ -4,29 +4,28 @@ import { segment } from "oicq";
 import lodash from "lodash";
 import command from '../components/command.js'
 import fs from 'fs';
-const dirpath = "plugins/lin-plugin/data/test";//文件夹路径
 
 const BotName = global.Bot.nickname;
 // 机器人名字，推荐不改(机器人如果换名字了需要重启来刷新)
-var def_gailv = await command.getConfig("ai_cfg", "gailv");
-var def_gailv_ = await command.getConfig("ai_cfg", "gailv_");
-var gailv = def_gailv
-var gailv_ = def_gailv_
+var def_gailv = await command.getConfig("ai_cfg", "def_gailv");
+var def_gailv_ = await command.getConfig("ai_cfg", "def_gailv_");
+var def_ai_now = await command.getConfig("ai_cfg", "def_ai_now");
+var def_onlyReplyAt = await command.getConfig("ai_cfg", "def_onlyReplyAt");
+var def_open = await command.getConfig("ai_cfg", "def_open");
 // 读yaml文件里面的设置的初始回复概率
-var sz = "";
-var msgsz = "";
 //这两个是与概率有关的
 var ai_api = await command.getConfig("ai_cfg", "ai_api");
 var ai_name = await command.getConfig("ai_cfg", "ai_name");
 var ai_nick = await command.getConfig("ai_cfg", "ai_nick");
-// 读取api接口
-var ai_now = 0;//正在用的接口
-// 定义现在正在用的接口赋初值
-var onlyReplyAt = true
 //群聊是否只关注@信息
-
-
-
+const dirpath = "plugins/lin-plugin/data";//文件夹路径
+const filename = `ai.json`;//文件名
+var Template = {//创建该用户
+    "gailv": def_gailv,
+    "open": def_open,
+    "onlyReplyAt": def_onlyReplyAt,
+    "ai_now": def_ai_now
+};
 var bad2good = {
     "傻逼": ["天使", "大可爱"],
     "去死": ["去玩", "去打电动"],
@@ -78,26 +77,49 @@ export class ai extends plugin {
         if (!e.msg) return false;
         //e.msg 用户的命令消息
         console.log("用户命令：", e.msg);
+        //---------------------------------------------------
+        if (!fs.existsSync(dirpath)) {//如果文件夹不存在
+            fs.mkdirSync(dirpath);//创建文件夹
+        }
+        if (!fs.existsSync(dirpath + "/" + filename)) {//如果文件不存在
+            fs.writeFileSync(dirpath + "/" + filename, JSON.stringify({//创建文件
+            }));
+        }
+        var json = JSON.parse(fs.readFileSync(dirpath + "/" + filename, "utf8"));//读取文件
+
+        if (e.isGroup) {
+            var id = e.group_id
+        }
+        else if (e.isPrivate) {
+            var id = e.user_id
+        }
+        if (!json.hasOwnProperty(id)) {//如果json中不存在该用户
+            json[id] = Template
+        }
+        let gailv = json[id].gailv
+        let open = json[id].open
+        let onlyReplyAt = json[id].onlyReplyAt
+        let ai_now = json[id].ai_now
+
+        //---------------------------------------------------
         //一个控制ai回复概率的模块
         if (e.isMaster) {
             //控制接口-------------------------------------------
-            let num = ai_api.length - 1
+            let api_num = ai_api.length - 1//接口数量
             // 发送当前的接口名字
             if (e.msg.includes('ai设置接口') || e.msg.includes('设置ai接口') || e.msg.includes('切换ai接口')) {
                 let message = e.msg.replace(/(ai设置接口|设置ai接口|切换ai接口|#)/g, "").replace(/[\n|\r]/g, "，").trim();//防止把内容里面的一下删了
-                if (message <= num && message >= 1 && !isNaN(message))//判断是不是api个数里面的,是则返回
+                if (message <= api_num && message >= 1 && !isNaN(message))//判断是不是api个数里面的,是则返回
                 {
                     ai_now = message - 1
                     e.reply(`已切换到${ai_now + 1}号接口${ai_name[ai_now]},接口链接已隐藏。`);
                 }
                 else {
-                    e.reply(`接口序号${message}超出范围或不合规，目前总量${num}`)
+                    e.reply(`接口序号${message}超出范围或不合规，目前总量${api_num}`)
                 }
-                return true;
             }
-            //设置概率-----------------------------------------
-            if (e.msg.includes('ai设置概率') || e.msg.includes('设置ai概率') || e.msg.includes('设置回复概率')) {
-                if (gailv <= 0) {
+            else if (e.msg.includes('ai设置概率') || e.msg.includes('设置ai概率') || e.msg.includes('设置回复概率')) {
+                if (!open) {
                     e.reply("ai已关闭,请先开启")
                 }
                 msgsz = e.msg.replace(/(ai设置概率|设置ai概率|设置回复概率|#)/g, "").replace(/[\n|\r]/g, "，").trim()
@@ -109,88 +131,76 @@ export class ai extends plugin {
                         e.reply("数值不在有效范围内,请输入0以上100以内的整数")
                     }
                     else {
-                        sz = Math.round(msgsz)
+                        let sz = Math.round(msgsz)
                         gailv = sz
                         e.reply(`已四舍五入设置ai触发概率：${gailv}%，`)
                     }
                 }
-                return true;
             }
-            if (e.msg.includes('ai关闭')) {
-                if (gailv <= 0) {
+            else if (e.msg.includes('ai关闭')) {
+                if (!open) {
                     e.reply("ai已经是关闭状态了")
-                    return true;
                 }
-                gailv = 0
+                open = false
                 e.reply("ai成功关闭")
-                return true;
             }
-            if (e.msg.includes('ai开启')) {
-                if (gailv == 0) {
-                    gailv = def_gailv
-                    e.reply(`成功开启,目前ai触发概率:${gailv}%!`)
-                    return true;
+            else if (e.msg.includes('ai开启')) {
+                if (!open) {
+                    open = true
+                    e.reply(`成功开启,目前ai触发概率为关闭时概率:${gailv}%!`)
                 }
-                e.reply(`ai已经是开启状态了,概率为${gailv}%`)
-                return true;
+                e.reply(`ai已经是开启状态了,不需要再开启一遍哦！`)
             }
-
-            if (e.msg.includes('只关注@消息')) {
+            else if (e.msg.includes('只关注@消息')) {
                 onlyReplyAt = true;
                 e.reply("好啦，现在只回复@消息了")
-                return true;
             }
-            if (e.msg.includes('关注所有消息')) {
+            else if (e.msg.includes('关注所有消息')) {
                 onlyReplyAt = false;
                 e.reply("现在我会关注每一条消息了")
-                return true;
             }
-            if (e.msg == '太安静了') {
-                //如果概率增加后大于100
-                if (gailv + gailv_ > 100) {
-                    e.reply(`目前ai触发概率：${gailv}%，再加${gailv_}就溢出来了ヾ(≧▽≦*)o`)
-                    return
-                }
-                gailv += gailv_;
-                e.reply(`概率提升，目前ai触发概率：${gailv}%，`)
-                return true;
-            }
-            else
-                if (e.msg == '太吵了') {
-                    //如果概率等于0
-                    if (gailv > 0) {
-                        e.reply("ai已关闭,请先开启")
-                        return true;
+            else if (e.msg == '太安静了') {
+                if (open) {
+                    if (gailv + def_gailv_ > 100) {
+                        e.reply(`目前ai触发概率：${gailv}%，再加${def_gailv_}就溢出来了ヾ(≧▽≦*)o`)
+                        return
                     }
-                    if (gailv == 10) {
-                        //提示不能修改了
-                        e.reply("很安静了，再改就关掉了>_<");
-                        return true;
+                    gailv += def_gailv_;
+                    e.reply(`概率提升，目前ai触发概率：${gailv}%，`)
+                }
+                else {
+                    e.reply("ai是关闭状态呢")
+                }
+            }
+            else if (e.msg == '太吵了') {
+                //如果概率等于0
+                if (!open) {
+                    e.reply("ai已关闭,请先开启")
+                }
+                else {
+                    if (gailv - def_gailv_ < 0) {
+                        e.reply(`目前ai触发概率：${gailv}%，再减${def_gailv_}就关掉了>_<`)
                     }
                     else {
-                        gailv -= gailv_;
+                        gailv -= def_gailv_;
                         e.reply(`概率降低，目前ai触发概率：${gailv}%，`)
-                        return true;
                     }
                 }
-            //查看状态----------------------------------
-            if (e.msg.includes('ai状态')) {
-                e.reply(`目前ai触发概率：${gailv}%,是否需要@${onlyReplyAt},正在使用${ai_name[ai_now]}`)
-                return true;
             }
-            return false;
+            //查看状态----------------------------------
+            else if (e.msg.includes('ai状态')) {
+                e.reply(`目前ai触发概率：${gailv}%,是否需要@${onlyReplyAt},正在使用${ai_name[ai_now]},是否开机${open}`)
+            }
+            fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(json, null, "\t"));//写入文件
+            return true;
         }
-        if (e.msg.charAt(0) == '#') return false;
-        let b = Math.round(Math.random() * 100)
+        else if (e.msg.charAt(0) == '#') return false;
         //群聊是否需要消息中带有机器人昵称或者@机器人才触发
-        if ((e.msg.includes(BotName) || e.atme || e.isPrivate || !onlyReplyAt) && gailv >= b) {
+        if ((e.msg.includes(BotName) || e.atme || e.isPrivate || !onlyReplyAt) && gailv >= Math.round(Math.random() * 100)) {
             console.log("ai消息：", e.msg);
-            //接收时将机器人名字替换为青云客AI的菲菲
+            //接收时将机器人名字替换为对应ai的名字
 
-
-            let message = e.msg.trim().replace(eval(`/${BotName}/g`), "菲菲").replace(/[\n|\r]/g, "，");
-            //这里需要处理一下，先埋个坑
-
+            let message = e.msg.trim().replace(eval(`/${BotName}/g`), `${ai_nick[ai_now]}`).replace(/[\n|\r]/g, "，");
 
             //抓取消息并转换为Json
             let postUrl = `${ai_api[ai_now]}${message}`;
@@ -199,20 +209,18 @@ export class ai extends plugin {
 
             let replyData = await response.json();//将返回的数据转化为json文件
 
-
             //******************************//
-            //将返回的数据转化为的json文件保存研究         
-            var date = new Date();
-            let filename = `ai_test_${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}-${date.getMinutes}-${date.getSeconds}-ai.json`
-            if (!fs.existsSync(dirpath)) {//如果文件夹不存在
-                fs.mkdirSync(dirpath);//创建文件夹
-            }
-            if (!fs.existsSync(dirpath + "/" + filename)) {//如果文件不存在
-                fs.writeFileSync(dirpath + "/" + filename, JSON.stringify({//创建文件
-                }));
-            }
-            fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(replyData, null, "\t"));//写入文件
-
+            // //将返回的数据转化为的json文件保存研究         
+            // var date = new Date();
+            // let filename = `ai_test_${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}-${date.getMinutes}-${date.getSeconds}-ai.json`
+            // if (!fs.existsSync(dirpath)) {//如果文件夹不存在
+            //     fs.mkdirSync(dirpath);//创建文件夹
+            // }
+            // if (!fs.existsSync(dirpath + "/" + filename)) {//如果文件不存在
+            //     fs.writeFileSync(dirpath + "/" + filename, JSON.stringify({//创建文件
+            //     }));
+            // }
+            // fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(replyData, null, "\t"));//写入文件
 
             let replyMsg = [];//这个保存返回信息里面的文本文件
             replyData = JSON.stringify(replyData) //转换字符串用于判断返回值
