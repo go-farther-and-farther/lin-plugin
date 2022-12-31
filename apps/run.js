41c78588912848b4b9296ea34e52d0fd7f473511
go-farther-and-farther <2859167710@qq.com>
2022-12-31 12:27:38 +0800

    modified:   apps/run.js

--- apps/run.js
+++ apps/run.js
@@ -162,7 +162,7 @@ export class run extends plugin {
         } else {//如果在跑路列表中
           json2[e.group_id].shield = json2[e.group_id].shield2.filter(item => item != e.at)
           json2 = await lin_data.getuser(id, json2, 'run', Template, true)//只在群聊有效
-          e.reply(`本群你已经被解除屏蔽了！`);//回复消息
+          e.reply(`本群${e.at}已经被解除屏蔽了！`);//回复消息
           return true;//拦截指令
         }
       }
