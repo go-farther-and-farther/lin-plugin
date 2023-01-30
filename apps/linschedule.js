d3f4cfbd8bf4a58a94a0cdf683700cff4e19f142
tyg211375 <2113752439@qq.com>
2023-01-30 14:50:08 +0800

    全员点赞添加配置与提示

--- apps/linschedule.js
+++ apps/linschedule.js
@@ -1,8 +1,9 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import schedule from "node-schedule";
 import command from '../components/command.js'
-import cfg from '../../../lib/config/config.js'
-var thumbUptime = 21//Number(await command.getConfig("thumbUp_cfg", "time"));//这个是开始时间
+import yzcfg from '../../../lib/config/config.js'
+var thumbUptime = 12//自动点赞开始时间
+var thumuppen = await command.getConfig("thumbUp_cfg", "thumuppen")
 export class thumbUp extends plugin {
 	constructor() {
 		super({
@@ -29,7 +30,6 @@ export class thumbUp extends plugin {
 	 * @param e oicq传递的事件参数e
 	 */
 	async thumbUp(e) {
-		e.reply('开始了哦！')
 		thumbUp_start('hand');
 	}
 }
@@ -37,13 +37,12 @@ export class thumbUp extends plugin {
 schedule.scheduleJob('0 0 * * * *', async () => {
 	let time = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
 	let hour = new Date(time).getHours()
-	if (hour == thumbUptime)
+	if (hour == thumbUptime && thumuppen)
 		thumbUp_start('auto');
 }
 );
 
 async function thumbUp_start(key) {
-	console.log(`开始给所有好友点赞点赞,正在点赞中...`)
 	let friendmap = Bot.fl
 	var arr = []
 	for (let friend of friendmap) {
@@ -51,22 +50,32 @@ async function thumbUp_start(key) {
 	}
 	arr = arr.filter(item => item != Bot.uin)
 	if (key == 'auto'){
+        for(let mat of yzcfg.masterQQ){
+            await common.relpyPrivate(mat, `自动点赞任务开始\n本次点赞好友共${arr.length}位\n自动点赞为防止风险,间隔较长,预计${arr.length * 2}分钟完成\n自动开关请前往本插件config/lin.config.yaml中修改`)
+        }
     	var s = 1000
 		for (let i = 0; i < arr.length - 1; i++) {
-			let a = Math.round(Math.random() * 5 + 1) * 600000
+			let a = Math.round(Math.random() * 5 + 1) * 60000
 			let sj = s
 		    s += a
 			setTimeout(() => {
 			    console.log(`本次为开始后${sj}毫秒,自动第${i}次点赞.点赞对象${arr[i]},下次点赞是${a}毫秒后`)
 			    Bot.pickFriend(arr[i]).thumbUp(10);//点赞10次，默认没有svip
-			}, sj);//随机延时,十到六十分钟
+			}, sj);//随机延时,一到五分钟
 		}
 	}
-	if (key == 'hand')
-		for (let i = 0; i < arr.length - 1; i++) {
+	if (key == 'hand'){
+        for(let mat of yzcfg.masterQQ){
+            await common.relpyPrivate(mat, `点赞开始,本次点赞好友共${arr.length}位,预计${arr.length * 10}秒完成`)
+        }
+     	for (let i = 0; i < arr.length; i++) {
+			if(i < arr.length)
 			setTimeout(() => {
 				console.log(`本次为手动第${i}次点赞.点赞对象${arr[i]}`)
 				Bot.pickFriend(arr[i]).thumbUp(10);//点赞10次，默认没有svip
 			}, i * 10000);//10秒延时
+			if(i == arr.length)
+			this.reply(`点赞任务完成`)
 		}
+	}
 }
