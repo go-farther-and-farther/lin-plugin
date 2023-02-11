774ccf30c2875546b056d7190a35e2e01ac72a35
tyg211375 <2113752439@qq.com>
2023-02-11 22:29:32 +0800

    fix

--- apps/user/run.js
+++ apps/user/run.js
@@ -54,7 +54,7 @@ export class runset extends plugin {
       /** https://oicqjs.github.io/oicq/#events */
       event: 'message',
       /** 优先级，数字越小等级越高 */
-      priority: 2,
+      priority: 0,
       rule: [
         {
           /** 命令正则匹配 */
